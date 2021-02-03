a0cb5e6413b4d9eba9e9e8e0af45d50f93a132da
zhu <zhuws@jwitsec.com>
2021-02-03 18:48:37 +0800

    添加moke

---
+++ src/app/share/moke/roles-moke.ts
@@ -0,0 +1,7 @@
+import {Roles} from "./roles";
+
+export const RoleList: Roles[] = [
+  {id_:1,role_name:'ROLE_ROOT', role_name_zh: '超级管理员'},
+  {id_:2,role_name:'ROLE_ADMIN', role_name_zh: '管理员'},
+  {id_:3,role_name:'ROLE_GUEST', role_name_zh: '普通用户'},
+];
---
+++ src/app/share/moke/roles.ts
@@ -0,0 +1,5 @@
+export interface Roles {
+  id_: number;
+  role_name: string;
+  role_name_zh: string;
+}
